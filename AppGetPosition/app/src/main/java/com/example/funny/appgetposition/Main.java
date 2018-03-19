de8540b037f7ca06c6f1502f53265b6e4798aa07
Yaroslav <Funny@localhost.localdomain>
2018-03-19 17:16:19 +0300

    first commit

--- AppGetPosition/app/src/main/java/com/example/funny/appgetposition/Main.java
+++ AppGetPosition/app/src/main/java/com/example/funny/appgetposition/Main.java
@@ -1,25 +1,56 @@
 package com.example.funny.appgetposition;
 
+import android.Manifest;
 import android.app.TabActivity;
+import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.PatternMatcher;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.internal.SnackbarContentLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.util.zip.Inflater;
 
 
-public class Main extends TabActivity{
+public class Main extends TabActivity implements TabHost.OnTabChangeListener {
 
-
+    TabHost tabHost;
+    WebView web;
+    ConstraintLayout constraintLayout;
     String whereIMm = "Где я";
-
+    Double latitude,longitude;
+    String _lat,_long;
+    public static final int BEGIN_LATITUDE=33,BEGIN_LONGTITUDE=54,FINAL_LATITUDE=44,FINAL_LONGTITUDE=65;
     static {
         System.loadLibrary("native-lib");
     }
@@ -30,7 +61,7 @@ public class Main extends TabActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        TabHost tabHost = getTabHost();
+        tabHost = getTabHost();
 
         TabHost.TabSpec tabSpec;
 
@@ -42,7 +73,10 @@ public class Main extends TabActivity{
         tabSpec = tabHost.newTabSpec("tag2");
         tabSpec.setIndicator(whereIMm);
         tabSpec.setContent(tabFactory);
+        tabHost.setOnTabChangedListener(this);
         tabHost.addTab(tabSpec);
+
+        constraintLayout = findViewById(R.id.Constlay);
 
 
 
@@ -56,26 +90,80 @@ public class Main extends TabActivity{
         public View createTabContent(String tag) {
 
             if (tag == "tag2") {
-           WebView web = new WebView(Main.this);
+
+                web = new WebView(Main.this);
+                web.setWebViewClient(new WebViewClient());//search?q=56.30286133+44.02253449
+                WebSettings webSettings = web.getSettings();
+                webSettings.setJavaScriptEnabled(true);
+                webSettings.setSupportZoom(true);
+                webSettings.setGeolocationEnabled(true);
+                webSettings.getLoadWithOverviewMode();
+
            return web;
 
             }
             return null;
         }
+
     };
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
-        menu.add(0,0,0,"Выйти");
+        menu.add(0,R.integer.item0,0,"Настройки");
+        menu.add(0,R.integer.item1,0,"Выйти");
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.integer.item0:
+            {
+                Intent intent =  new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,Uri.parse("package:"+ getPackageName()));
+                startActivity(intent);
+                break;
+            }
+            case R.integer.item1:
 
-        finish();
+            {
+                finish();
+            }
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1)
+        {
+
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION))
+                {
+                    Toast.makeText(this,"Пожалуйста подтвердите разрешение",Toast.LENGTH_SHORT).show();
+                }
+
+                else
+                {
+                    Snackbar.make(constraintLayout,"Проверьте настройки",Snackbar.LENGTH_LONG)
+                            .setAction("Настройки", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Intent intent =  new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,Uri.parse("package:" + getPackageName()));
+                                    startActivity(intent);
+
+                                }
+                            })
+                            .show();
+                }
+            }
+
+        }
     }
 
     /**
@@ -84,5 +172,37 @@ public class Main extends TabActivity{
      */
     public native String stringFromJNI();
 
+    @Override
+    public void onTabChanged(String tabId) {
+        switch (tabId)
+        {
+            case "tag2":
+            {
+                File file = new File(getFilesDir().getAbsolutePath().toString() + File.separator + "test" + File.separator + "test.txt");
+                try {
+                    FileReader fr = new FileReader(file);
+                    BufferedReader br = new BufferedReader(fr);
+                    String d = "";
+                        while (br.ready() == true) {
+                        d = br.readLine();
+                         }
+                    Log.d("LOG_TEST: ", d);
+                    _lat = d.substring(BEGIN_LATITUDE,FINAL_LATITUDE);
+                    _long = d.substring(BEGIN_LONGTITUDE,FINAL_LONGTITUDE);
+                    br.close();
+                    fr.close();
+                    Log.d("LOG_TEST: ", _lat + " " + _long);
+                } catch (FileNotFoundException e) {
+                    Toast.makeText(getBaseContext(),"Последних данных не было",Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                latitude = Double.valueOf(_lat);
+                longitude = Double.valueOf(_long);
+                web.loadUrl("https://www.google.ru/maps/search/"+ latitude + " " + longitude);
+                break;
+            }
+        }
+    }
 
 }
--- AppGetPosition/app/src/main/java/com/example/funny/appgetposition/One.java
+++ AppGetPosition/app/src/main/java/com/example/funny/appgetposition/One.java
@@ -1,155 +1,158 @@
 package com.example.funny.appgetposition;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.TabActivity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.icu.text.SimpleDateFormat;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
+import java.util.Date;
 
 
 public class One extends Activity implements View.OnClickListener, LocationListener {
 
     TextView whereIAm;
-    CheckBox chGPS, chInternet;
+    CheckBox chGPS;
     Button bAsk;
+    EditText eTime;
     LocationManager locationManager;
-    Location loc ;
+    Location loc;
     double osH, osW;
-
-    String[] perm = {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION};
-
+    SimpleDateFormat sdf;
+    String[] perm = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+    String time;
+    @TargetApi(Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.one);
 
         whereIAm = findViewById(R.id.twhereAmI);
+        whereIAm.setOnClickListener(this);
+        eTime = findViewById(R.id.eTime);
         chGPS = findViewById(R.id.chGPS);
         chGPS.setOnClickListener(this);
-        chInternet = findViewById(R.id.chInet);
         bAsk = findViewById(R.id.bAsk);
         bAsk.setOnClickListener(this);
 
+        sdf = new SimpleDateFormat("dd:HH:mm");
+        time = sdf.format(new Date());
+    }
 
 
 
-
-        if (chInternet.isChecked()) {
-
-        }
-
-
-
-
-
-
-        }
-
-
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onClick(View v) {
-        switch (v.getId())
-        {
-            case R.id.bAsk:
-            {
+        switch (v.getId()) {
+            case R.id.bAsk: {
                 whereIAm.setText("Location: Широта: " + osH + ";" + "Долгота:" + osW);
-
                 break;
             }
 
-            case R.id.chGPS:
-            {
-                if(!chGPS.isChecked()) {
-                    if (ContextCompat.checkSelfPermission(this, perm[0]) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, perm[1]) == PackageManager.PERMISSION_GRANTED) {
-                        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                        loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            case R.id.chGPS: {
+                if (chGPS.isChecked()) {
+                    requestGPS();
+                } else {
 
-                        if (loc != null) {
-                            double ds = loc.getLatitude();
-                            Log.i("latitude:", String.valueOf(ds));
-                        } else {
-                            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 1, this);
-                        }
-                        List<String> f = locationManager.getProviders(true);
-                        Log.i("providers", String.valueOf(f));
-
-
-                    }
-                }
-                else
-                {
-                    ActivityCompat.requestPermissions(this.getParent(),perm,1);
-                }
-
-                }
-                break;
-            }
-        }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == 1)
-        {
-            if (shouldShowRequestPermissionRationale(perm[0]))
-            {
-                Toast.makeText(getBaseContext(),"Пожалуйста подтвердите разрешение",Toast.LENGTH_SHORT);
-            }
-            else
-            {
-                Snackbar.make(new View(this),"Проверьте настройки",Snackbar.LENGTH_LONG)
-                        .setAction("Настройки", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                                Uri.parse("package:"+ getPackageName());
-
+                        if (checkSelfPermission(perm[0]) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(perm[1]) == PackageManager.PERMISSION_GRANTED)
+                        {
+                            try {
+                            locationManager.removeUpdates(this);
                             }
-                        });
+                            catch (Exception e) {}
+                            Toast.makeText(getBaseContext(), "Данные GPS не получаем", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(getBaseContext(), "Permission isn't granted", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                break;
             }
+            case R.id.twhereAmI:
+            {
+                TabActivity main = (TabActivity) this.getParent();
+                TabHost tabHost = main.getTabHost();
+                tabHost.setCurrentTabByTag("tag2");
+                break;
+            }
+
         }
     }
+
+
+    public void requestGPS() {
+        if (ContextCompat.checkSelfPermission(this, perm[0]) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, perm[1]) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this.getParent(), perm, 1);
+        } else {
+
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * 6, 1, this);
+            if (loc != null) {
+                osH = loc.getLatitude();
+                osW = loc.getLongitude();
+                Log.i("Location:", "Широта: " + osH + ";" + "Долгота:" + osW);
+
+            } else {
+
+                    loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    whereIAm.setText("Ожидание координат");
+
+            }
+
+
+        }
+
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putDouble("latitude",osH);
+        outState.putDouble("longitude",osW);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        osH = savedInstanceState.getDouble("latitude");
+        osW = savedInstanceState.getDouble("longitude");
+    }
+
 
     @Override
     public void onLocationChanged(final Location loc) {
-       osH = loc.getLatitude();
-       osW = loc.getLongitude();
-        Toast.makeText(getBaseContext(),"Location:" + osH+ ";" + osW,Toast.LENGTH_LONG).show();
-        Log.i("Location:", "Широта: " + osH + ";" + "Долгота:" + osW);
-        File file = new File(File.pathSeparator + "Coordinate's");
-        try {
-            FileOutputStream fileStream = openFileOutput(file.getPath(),Context.MODE_PRIVATE);
-            fileStream.write(("Location: Широта: " + osH + ";" + "Долгота:" + osW).getBytes());
-            fileStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+
+
+                    osH = loc.getLatitude();
+                    osW = loc.getLongitude();
+                    Toast.makeText(getBaseContext(), "Location:" + osH + ";" + osW, Toast.LENGTH_LONG).show();
+                    Log.i("Location:", "Широта: " + osH + ";" + "Долгота:" + osW);
+                    recInFile();
+                     eTime.setText(time);
+
+                }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -158,11 +161,29 @@ public class One extends Activity implements View.OnClickListener, LocationListe
 
     @Override
     public void onProviderEnabled(String provider) {
-        Toast.makeText(getBaseContext(),"GPS is Enabled",Toast.LENGTH_LONG).show();
+        Toast.makeText(getBaseContext(), "GPS is Enabled", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onProviderDisabled(String provider) {
-        Toast.makeText(getBaseContext(),"GPS is Disabled",Toast.LENGTH_LONG).show();
+        Toast.makeText(getBaseContext(), "GPS is Disabled", Toast.LENGTH_LONG).show();
+    }
+
+    private void recInFile()
+    {
+        File dir = new File(getFilesDir().getAbsolutePath().toString() + File.separator + "test");
+        if (!dir.exists())
+        {
+            dir.mkdirs();
+        }
+        File file = new File(dir,"test.txt");
+        try {
+            FileWriter fw = new FileWriter(file,true);
+            fw.append("Time: " + time + " Location: Широта: " + osH + " " + "Долгота: " + osW + "\n" );
+            fw.flush();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
